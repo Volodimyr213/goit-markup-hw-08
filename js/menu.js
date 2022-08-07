35e672ac5689cf4bf6231d0bd2318604f04749c5
Volodimyr213 <98879258+Volodimyr213@users.noreply.github.com>
2022-08-07 21:57:59 +0300

    task8-fix15

--- js/menu.js
+++ js/menu.js
@@ -6,8 +6,9 @@
       const expanded =
         menuBtnRef.getAttribute("aria-expanded") === "true" || false;
   
-      menuBtnRef.classList.toggle("no-scroll");
+      menuBtnRef.classList.toggle("is-open");
       menuBtnRef.setAttribute("aria-expanded", !expanded);
+     
   
       mobileMenuRef.classList.toggle("is-open");
      
